fixed sign in the angle conversion function

estimate_pose.py
@@ -166,11 +166,15 @@ def convert_for_export(global_pose):
     r =  Rotation.from_matrix(global_pose[:3, :3])
     # angles are euler angles in RADIANS
     angles = r.as_rotvec()
+    print(angles)
     x = global_pose[2,3]
     y = 8.2042 - global_pose[0, 3]
-    pose = np.array([x, y, 2 * math.pi - angles[1]])
+    ret_angle = angles[1]
+    if(ret_angle < 0):
+        ret_angle = 2*math.pi + ret_angle
+    
+    pose = np.array([x, y, 2 * math.pi - ret_angle])#angle is negative
     return pose
-
 
 #####################################
 if __name__ == "__main__":
@@ -187,9 +191,8 @@ if __name__ == "__main__":
     
     multiplied = np.matmul(g_to_t, t_to_r)
 
-    test_pose = np.array([[-1,0,0,1],
-                            [0,1,0,2],
-                            [0,0,-1,3]])
+    test_pose = get_transformation_matrix(rotation_by_240d, np.array([[1, 2, 3]]).T)
+    #print(test_pose[:3, :3])
 
     conv = convert_for_export(test_pose)
 