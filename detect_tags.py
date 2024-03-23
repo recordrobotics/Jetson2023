3199fc35dc3b3181d0e74465a7dade9a3aa60190
scaryjoshie <scaryjoshie@gmail.com>
2024-03-23 16:46:19 -0400

    pushes

--- detect_tags.py
+++ detect_tags.py
@@ -39,7 +39,7 @@ def detect_tag(frame, DETECTION_MARGIN_THRESHOLD = 40):
     filter_tags = [tag for tag in tags if 
                    tag.getDecisionMargin() > DETECTION_MARGIN_THRESHOLD and tag.getId() in tagTransforms.keys()]
 
-    tag_poses = [(tag.getId(), estimator.estimateOrthogonalIteration(tag, 50).pose2) for tag in filter_tags]
+    tag_poses = [(tag.getId(), estimator.estimateOrthogonalIteration(tag, 50).pose1) for tag in filter_tags]
     #tag_poses.sort(key = lambda tuple: tuple[1].translation().dis)
     if len(tag_poses) > 0:
         # Gets first of filter_tags
@@ -49,5 +49,6 @@ def detect_tag(frame, DETECTION_MARGIN_THRESHOLD = 40):
         # Gets tag pose
         tag_pose = tag[1]
         # Returns tag pose and ID
+        print(tag_pose.X(), tag_pose.Y())
         return tag_pose, tag_id
     return None, None
